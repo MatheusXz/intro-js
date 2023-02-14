24b6f0e3225b8ce6149d79157bc791ea0d354e28
MatheusXz <matheus000000018@gmail.com>
2023-02-13 22:50:44 -0300

    Aula 1
    Teste

---
+++ exemplosjs/001-helloWorld.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+let nome = 'Matheus'
+let idade = 22
+let trabalha = true
+let altura = 1.87
+
+new Array(coresFavoritas) = ['roxo', 'azul', 'preto']
+let endereco = { 
+    logradouro: 'Rua',
+    nomeLograd: 'Tal',
+    numLograd:  '1258-A',
+    bairro:     'Centro'
+}
+
+let covid = null
+let dengue
+
+
+const nums = [9, 15, 45, 18, 32, 12354, 30, 29];
+
+const alunos = {
+    nome: 'Matheus',
+    idade: 22,
+    notas: [9.6, 5.3, 8.6, 7]
+}
+
+
+console.log(`
+    Meu nome é ${nome} tenho ${idade} minha altura é de ${altura} meu endereço é 
+`)
+console.log(altura)
+console.log(nums.filter(n => n >= 30))
+console.log(typeof(coresFavoritas))
+// console.log(alunos.map(n => n))
